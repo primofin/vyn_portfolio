0a77cbe8b342ee2efd09a9afdc6d0cdc9cf1b084
Vy Nguyen <vyn@metropolia.fi>
2020-02-20 01:45:16 +0200

    Updates

---
+++ precache-manifest.f78b21ba21b24547a7a52441dd9d3e5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6cddc35d7e7cd77c9382742e634619c",
+    "url": "/vyn_portfolio/index.html"
+  },
+  {
+    "revision": "8f849a439bdb4ddfa1bf",
+    "url": "/vyn_portfolio/static/css/main.fbcf09d2.chunk.css"
+  },
+  {
+    "revision": "06f1f6ef433d6d067a75",
+    "url": "/vyn_portfolio/static/js/2.c0e81d62.chunk.js"
+  },
+  {
+    "revision": "7d4610b035b1afebd39e5e8fd5e70ce7",
+    "url": "/vyn_portfolio/static/js/2.c0e81d62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f849a439bdb4ddfa1bf",
+    "url": "/vyn_portfolio/static/js/main.9b3cd4e1.chunk.js"
+  },
+  {
+    "revision": "1bf894cb4af52e2357aa",
+    "url": "/vyn_portfolio/static/js/runtime-main.04d50875.js"
+  }
+]);
